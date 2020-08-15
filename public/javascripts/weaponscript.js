9b6145e05484f9430fca688c13c0a3a57c705c69
Aleks <bobopirate1010@gmail.com>
2020-08-14 17:29:24 -0700

    Weapon shit

--- public/javascripts/weaponscript.js
+++ public/javascripts/weaponscript.js
@@ -14,3 +14,29 @@ background.onload = function(){
     ctx.drawImage(background,0,0);   
     
 }
+
+
+function refresh(){
+    
+
+    var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d");
+
+    canvas.width = 800;
+    canvas.height = 890;
+
+
+    var background = new Image();
+    background.src = "/images/weapon_subpart.png#" + new Date().getTime();
+
+    console.log("Refreshing!");
+    screensrc = document.getElementById('screen').getAttribute('src');
+
+    // Make sure the image is loaded first otherwise nothing will draw.
+    background.onload = function(){
+        ctx.drawImage(background,0,0);   
+        
+    }
+}
+  
+  setInterval(refresh, 2*1000);
