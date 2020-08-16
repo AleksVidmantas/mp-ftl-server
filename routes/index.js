bfc04e9347e181dcc091d253ce7918549935a46e
Aleks <bobopirate1010@gmail.com>
2020-08-16 12:08:59 -0700

    hotfix#14

--- routes/index.js
+++ routes/index.js
@@ -37,25 +37,30 @@ router.post('/image', function(req,res,next){
 
         //relocate image from tmp into project
       fs.rename(files.image.path, './public/images/screenshot.png', function (err) {
-      
-        if (err)  throw err;
-        sharp('./public/images/weapon_subpartcopy.png').extract({ width: 800, height: 890, left: 1120, top: 0 }).toFile('./public/images/weapon_subpart.png').then(function(new_file_info) {
-          console.log("Image cropped and saved");
-          })
-      .catch(function(err) {
-          console.log(err);
-      });
+        fs.copyFile('./public/images/screenshot.png', './public/images/weapon_subpartcopy.png', (err) => {
+          if (err) throw err;
+            console.log(err);
+            if (err)  throw err;
+            let imgSS = fs.createWriteStream('./public/images/weapon_subpartcopy.png')
+            imgSS.on('close', function(path){
+                sharp('./public/images/weapon_subpartcopy.png').extract({ width: 800, height: 890, left: 1120, top: 0 }).toFile('./public/images/weapon_subpart.png').then(function(new_file_info) {
+                  console.log("Image cropped and saved");
+                  })
+              .catch(function(err) {
+                  console.log(err);
+              });
+            })
+    
+  
+      //    console.log('source.txt was copied to destination.txt');
+          
+        });
+        
+       
             
       });
 
-      fs.copyFile('./public/images/screenshot.png', './public/images/weapon_subpartcopy.png', (err) => {
-        if (err) throw err;
-          console.log(err);
-        
-
-    //    console.log('source.txt was copied to destination.txt');
-        
-      });
+      
     //  });
       
     });
