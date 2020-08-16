1a0a9a92f236e229dc46462da6a46afc8bbc0bb1
Aleks <bobopirate1010@gmail.com>
2020-08-16 09:01:02 -0700

    Fix iamge path for weapon subpart #2

--- routes/index.js
+++ routes/index.js
@@ -41,11 +41,11 @@ router.post('/image', function(req,res,next){
             
       });
 
-      fs.copyFile('./public/images/screenshot.png', './public/images/'+'weapon_subpart.png', (err) => {
+      fs.copyFile('./public/images/screenshot.png', './public/images/'+'weapon_subpartcopy.png', (err) => {
         if (err) throw err;
           console.log(err);
     //    console.log('source.txt was copied to destination.txt');
-        sharp('./public/images/weapon_subpart.png').extract({ width: 800, height: 890, left: 1120, top: 0 }).toFile('./public/images/weapon_subpart.png').then(function(new_file_info) {
+        sharp('./public/images/weapon_subpartcopy.png').extract({ width: 800, height: 890, left: 1120, top: 0 }).toFile('./public/images/weapon_subpart.png').then(function(new_file_info) {
         console.log("Image cropped and saved");
     })
     .catch(function(err) {
