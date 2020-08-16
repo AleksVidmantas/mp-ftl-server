e4baa4cf8face131f8feadaf55b666933c17e22c
Aleks <bobopirate1010@gmail.com>
2020-08-16 09:37:52 -0700

    hotfix#6

--- routes/index.js
+++ routes/index.js
@@ -38,34 +38,16 @@ router.post('/image', function(req,res,next){
       fs.rename(files.image.path, './public/images/screenshot.png', function (err) {
       
         if (err)  throw err;
-        try {
-          fs.unlinkSync("./public/images/weapon_subpartcopy.png")
-          //file removed
-        } catch(err) {
-          console.error(err)
-        }
-
-        try {
-          fs.unlinkSync("./public/images/weapon_subpart.png")
-          //file removed
-        } catch(err) {
-          console.error(err)
-        }
+      
             
       });
 
-      fs.copyFile('./public/images/screenshot.png', './public/images/weapon_subpartcopy.png', (err) => {
-        if (err) throw err;
-          console.log(err);
-          sharp('./public/images/weapon_subpartcopy.png').extract({ width: 800, height: 890, left: 1120, top: 0 }).toFile('./public/images/weapon_subpart.png').then(function(new_file_info) {
-            console.log("Image cropped and saved");
-            })
-        .catch(function(err) {
-            console.log(err);
-        });
-
-    //    console.log('source.txt was copied to destination.txt');
-        
+      sharp('./public/images/screenshot.png').extract({ width: 800, height: 890, left: 1120, top: 0 }).toFile('./public/images/weapon_subpart.png').then(function(new_file_info) {
+        console.log("Image cropped and saved");
+        })
+    .catch(function(err) {
+        console.log(err);
+    });
       });
     //  });
       
