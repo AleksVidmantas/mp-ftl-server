394cf9426d6bb7ed153082c25b27108bcb24fed7
Aleks <bobopirate1010@gmail.com>
2020-08-16 06:50:36 -0700

    Fix image flicker (wooohooo(

--- public/javascripts/weaponscript.js
+++ public/javascripts/weaponscript.js
@@ -12,6 +12,15 @@ background.src = "/images/weapon_subpart.png#" + new Date().getTime();
 // Make sure the image is loaded first otherwise nothing will draw.
 background.onload = function(){
     ctx.drawImage(background,0,0);   
+    setInterval(function(){
+        var background = new Image();
+        background.src = "/images/weapon_subpart.png#" + new Date().getTime();
+        background.onload() = function(){
+            requestAnimationFrame(function(){
+                ctx.drawImage(background,0,0); 
+            })
+        }
+    }, 5*1000);
     
 }
 
@@ -25,18 +34,21 @@ function refresh(){
     canvas.width = 800;
     canvas.height = 890;
 
-
-    var background = new Image();
-    background.src = "/images/weapon_subpart.png#" + new Date().getTime();
+    
+    
+    ctx.drawImage(background,0,0);   
 
     console.log("Refreshing!");
     
 
-    // Make sure the image is loaded first otherwise nothing will draw.
-    background.onload = function(){
-        ctx.drawImage(background,0,0);   
+    // // Make sure the image is loaded first otherwise nothing will draw.
+    // background.onload = function(){
+    //     requestAnimationFrame(function(){
+            
+
+    //     })
         
-    }
+    // }
 }
   
-  setInterval(refresh, 5*1000);
+ 
