6d40bc6b7d257b083e4bc6953dfd9a14457b7e8c
Aleks <bobopirate1010@gmail.com>
2020-08-16 17:51:59 -0700

    Add functional tactical code#2

--- public/javascripts/tacticalscript.js
+++ public/javascripts/tacticalscript.js
@@ -1,10 +1,9 @@
 
 var canvas = document.getElementById("canvas"),
-ctx = canvas.getContext("2d");
+    ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 890;
-let mCoords = [0,0]
 
 
 var background = new Image();
@@ -12,24 +11,23 @@ background.src = "/images/weapon_subpart.png?" + new Date().getTime();
 
 // Make sure the image is loaded first otherwise nothing will draw.
 background.onload = function(){
-ctx.drawImage(background,0,0);   
-setInterval(function(){
-    var background = new Image();
-    background.src = "/images/weapon_subpart.png?" + new Date().getTime();
-    background.onload = function(){
-        requestAnimationFrame(function(){
-            ctx.drawImage(background,0,0); 
-            ctx.save();
-            // ctx.translate(beamVals[0],beamVals[1])
-            // ctx.rotate(beamVals[2])
-            // ctx.drawImage(beamComplete,0,0)
-            ctx.restore();
-        })
-    }
-}, 8000);
-
+    ctx.drawImage(background,0,0);   
+    setInterval(function(){
+        var background = new Image();
+        background.src = "/images/weapon_subpart.png?" + new Date().getTime();
+        background.onload = function(){
+            requestAnimationFrame(function(){
+                ctx.drawImage(background,0,0); 
+                // ctx.save();
+                // ctx.translate(beamVals[0],beamVals[1])
+                // ctx.rotate(beamVals[2])
+                // ctx.drawImage(beamComplete,0,0)
+                // ctx.restore();
+            })
+        }
+    }, 800);
+    
 }
-
 
 function refresh(){
 
